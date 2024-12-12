7a3a8932543b5f583f4a9df550703d284373437a
Raisson Souza <raissonrai@gmail.com>
2024-12-12 11:35:55 -0300

    - Adicionada logo da instituição; - Utilizado InitialContext para apresentação e carregamento da aplicação.

--- contexts/InitialContext.tsx
+++ contexts/InitialContext.tsx
@@ -1,6 +1,9 @@
+import { CustomImage } from "@/components/customs/CustomImage"
 import { Screen } from "../components/base/Screen"
+import Box from "@/components/base/Box"
 import Loading from "../components/base/Loading"
-import React, { createContext, useContext, useState } from "react"
+import React, { createContext, useContext, useEffect, useState } from "react"
+import TextBold from "@/components/base/TextBold"
 
 type InitialContextProps = {
     children: JSX.Element | JSX.Element[]
@@ -12,12 +15,26 @@ const InitialContext = createContext<InitialContext | null>(null)
 
 /** Context especializado no tratamento / carregamento de dados iniciais da aplicação */
 export default function InitialContextComponent({ children }: InitialContextProps) {
-    const [ loading, setLoading ] = useState<boolean>(false)
+    const [ loading, setLoading ] = useState<boolean>(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1500)
+    }, [])
 
     if (loading) {
         return (
             <Screen>
-                <Loading />
+                <Box.Column
+                    style={{
+                        gap: 3,
+                    }}
+                >
+                    <CustomImage.Local filePathByRequire={ require("@/assets/images/logo_amf.png") } />
+                    <TextBold>Carteira Digital do Estudante</TextBold>
+                    <Loading />
+                </Box.Column>
             </Screen>
         )
     }
